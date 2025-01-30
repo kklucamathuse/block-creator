6774e5aaee7e93ec32cd93d2963c4dc74d13360f
Luca <luca.mathuse@kreativkarussell.de>
2025-01-30 12:08:09 +0100

    Fixed problem with gsub. Placeholders are successfully getting replaced by user input.

--- createblock.sh
+++ createblock.sh
@@ -34,11 +34,11 @@ create_file() {
 # Append content to the files
 append_file() {
     # templates=$("./file_templates.json")
-    template_php=$(jq -r '.["template.php"]' file_templates.json)
-    block_json=$(jq -r '.["block.json"]' file_templates.json)
+    template_php=$(jq '.["template.php"]' file_templates.json)
+    block_json=$(jq '.["block.json"]' file_templates.json)
 
     # Replace placeholders using jq
-    # block_json=$(jq '.["block.json"] | gsub("{{blockName}}"; "testName"))
+    block_json=$(jq -r '. | gsub("{{blockName}}"; "'"$blockName"'") | gsub("{{blockTitle}}"; "'"$blockTitle"'") | gsub("{{blockDescription}}"; "'"$blockDescription"'") | gsub("{{blockCategory}}"; "'"$blockCategory"'")' <<< "$block_json")
 
     echo "$template_php" > "$blockName/template.php"
     echo "$block_json" > "$blockName/block.json"
